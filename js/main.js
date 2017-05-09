b6ea9fe446a7263c9fbff277d5c5d4b83be2eeeb
Jacob Rhoda <jake@jadarstudios.com>
2017-05-09 00:09:54 -0500

    Fix navbar and add stuff

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 "use strict";
 
-function updateCoverImage(image) {
-	if(image.height > image.width) {
+function updateCoverImage(window, image) {
+	if(window.width < window.height) {
 	    image.height = "100%";
 	    image.width = "auto";
+	    console.log("doingthething!");
 	}
 }
 
@@ -16,15 +17,23 @@ $(document).ready(function () {
 			let image = elem.find("img");
 			          	  
 			let oVal = ((window_bottom - parent_top) / 3);
-			image.css("transform","translate3d(0px, " + oVal + "px, 0px)");
+			image.css("transform", "translate3d(0px, " + oVal + "px, 0px)");
         }); 
     });
 
     $(".hero").each(function() {
     	let image = $(this).find("img");
-    	updateCoverImage(image);
+    	updateCoverImage($(window), image);
     	$(window).resize(function() {
-    		updateCoverImage(image);
+    		updateCoverImage($(window), image);
     	});
+    });
+
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 350) {
+            $(".navbar-brand").css("opacity", "1");
+        } else {
+            $(".navbar-brand").css("opacity", "0");
+        }
     });
 });
