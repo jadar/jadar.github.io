ef0810276b2e3c270e2432b81b29e104f9170699
Jacob Rhoda <jake@jadarstudios.com>
2017-05-15 09:17:41 -0400

    Add final sections

--- js/main.js
+++ js/main.js
@@ -9,17 +9,17 @@ function updateCoverImage(window, image) {
 }
 
 $(document).ready(function () {
-	$(".parallax").each(function(){
-        let elem = $(this); // assigning the object
-        $(window).scroll(function() {
-            let parent_top = elem.offset().top;          
-			let window_bottom = $(window).scrollTop();
-			let image = elem.find("img");
+	// $(".parallax").each(function(){
+ //        let elem = $(this); // assigning the object
+ //        $(window).scroll(function() {
+ //            let parent_top = elem.offset().top;          
+	// 		let window_bottom = $(window).scrollTop();
+	// 		let image = elem.find("img");
 			          	  
-			let oVal = ((window_bottom - parent_top) / 3);
-			image.css("transform", "translate3d(0px, " + oVal + "px, 0px)");
-        }); 
-    });
+	// 		let oVal = ((window_bottom - parent_top) / 3);
+	// 		image.css("transform", "translate3d(0px, " + oVal + "px, 0px)");
+ //        }); 
+ //    });
 
     $(".hero").each(function() {
     	let image = $(this).find("img");
